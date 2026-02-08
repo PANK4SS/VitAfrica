f2c873938a54c9d22c5e3efd446e98e347cb4e5a
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-08 12:06:31 -0500

    ClientRequest.java dto complete

--- accesscore/src/main/java/com/pankassi/accesscore/dto/request/ClientRequest.java
+++ accesscore/src/main/java/com/pankassi/accesscore/dto/request/ClientRequest.java
@@ -1,4 +1,15 @@
 package com.pankassi.accesscore.dto.request;
 
-public record ClientRequest() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record ClientRequest(
+        @NotBlank(message = "The email is required")
+        @Email(message = "The Email must be an valide one")
+        String email,
+        @NotBlank(message = "The username is required")
+        String username,
+        @NotBlank(message="The password is required")
+        String password
+) {
 }
