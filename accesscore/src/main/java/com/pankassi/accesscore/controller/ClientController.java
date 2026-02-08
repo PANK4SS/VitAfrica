aae453b99c9d9ff5b90395e4200f75c6efb22979
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-08 17:59:02 -0500

    feat(controller): add endpoint for assigning roles to clients

--- accesscore/src/main/java/com/pankassi/accesscore/controller/ClientController.java
+++ accesscore/src/main/java/com/pankassi/accesscore/controller/ClientController.java
@@ -1,4 +1,25 @@
 package com.pankassi.accesscore.controller;
 
+import com.pankassi.accesscore.domain.model.Client;
+import com.pankassi.accesscore.dto.request.AssignRolesRequest;
+import com.pankassi.accesscore.service.ClientService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/clients")
+@RequiredArgsConstructor
 public class ClientController {
+    private final ClientService clientService;
+
+    @PostMapping("/{email}/roles")
+    public ResponseEntity<Client> assignRoles(
+            @PathVariable String email,
+            @Valid @RequestBody AssignRolesRequest request
+    ) {
+        Client updatedClient = clientService.assignRoles(email, request.roles());
+        return ResponseEntity.ok(updatedClient);
+    }
 }
