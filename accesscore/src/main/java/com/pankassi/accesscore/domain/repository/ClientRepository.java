c2b1c6fe4bdf8a0e13c99e1a6fa43ebf7ebd4eda
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-08 17:22:40 -0500

    fix(accesscore): rename existByEmail to existsByEmail in ClientService

--- accesscore/src/main/java/com/pankassi/accesscore/domain/repository/ClientRepository.java
+++ accesscore/src/main/java/com/pankassi/accesscore/domain/repository/ClientRepository.java
@@ -3,6 +3,9 @@ package com.pankassi.accesscore.domain.repository;
 import com.pankassi.accesscore.domain.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClientRepository extends JpaRepository<Client,Long> {
-    boolean existByEmail(String email);
+    boolean existsByEmail(String email);
+    Optional<Client> findByEmail(String email);
 }
