0722ec000e4d9acf7cae09bf1b862b929f1b3557
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-08 14:05:07 -0500

    refactor(dto): implement client to ClientResponse mapping logic

--- accesscore/src/main/java/com/pankassi/accesscore/dto/response/ClientResponse.java
+++ accesscore/src/main/java/com/pankassi/accesscore/dto/response/ClientResponse.java
@@ -1,10 +1,23 @@
 package com.pankassi.accesscore.dto.response;
 
+import com.pankassi.accesscore.domain.model.Client;
+
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public record ClientResponse(
     String clientName,
     String clientEmail,
     Set<String> roles
 ) {
+    //Convert a Client Into a ClientResponse
+    public static ClientResponse from(Client client){
+        return new ClientResponse(
+                client.getClientName(),
+                client.getEmail(),
+                client.getRoleSet()
+                    .stream()
+                    .map(role -> role.getRoleName())
+                    .collect(Collectors.toSet()));
+    }
 }
