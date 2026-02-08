4f513d27782d2bf9c2e00326b3b4d9af13e5183a
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-08 12:24:06 -0500

    Authentication service dependencies injection complete

--- accesscore/src/main/java/com/pankassi/accesscore/service/AuthenticationService.java
+++ accesscore/src/main/java/com/pankassi/accesscore/service/AuthenticationService.java
@@ -1,4 +1,21 @@
 package com.pankassi.accesscore.service;
 
+import com.pankassi.accesscore.domain.repository.ClientRepository;
+import com.pankassi.accesscore.domain.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthenticationService {
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Value("${app.default.role:USER}")
+    private String defaultRole;
 }
