598a0ee5150bf82db0eed65863c103892e58349a
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-08 12:57:04 -0500

    feat: implement client registration logic
    - Added registerClient to AuthenticationService - Included validation for client credentials - Integrated with the persistence layer

--- accesscore/src/main/java/com/pankassi/accesscore/service/AuthenticationService.java
+++ accesscore/src/main/java/com/pankassi/accesscore/service/AuthenticationService.java
@@ -1,6 +1,7 @@
 package com.pankassi.accesscore.service;
 
 import com.pankassi.accesscore.domain.model.Client;
+import com.pankassi.accesscore.domain.model.Role;
 import com.pankassi.accesscore.domain.repository.ClientRepository;
 import com.pankassi.accesscore.domain.repository.RoleRepository;
 import com.pankassi.accesscore.dto.request.ClientRequest;
@@ -9,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -22,20 +26,27 @@ public class AuthenticationService {
     private final PasswordEncoder passwordEncoder;
 
     @Value("${app.default.role:USER}")
-    private String defaultRole;
+    private String defaultRoleName;
 
     public Client registerClient(ClientRequest clientRequest){
         // Check if the user provide email already exist in DB
         if(clientRepository.existByEmail(clientRequest.email())){
             throw new IllegalStateException("The provided email is already used");
         }
+        //Collect default role by usign default role name
+        Role role = roleRepository.findByRoleName(defaultRoleName)
+                .orElseThrow(() -> new IllegalStateException(
+                        "Default role not found: " + defaultRoleName
+                ));
+
         // At the moment that the provided email don't exist
         Client client = new Client();
         client.setClientName(clientRequest.username());
         client.setEmail(clientRequest.email());
         client.setPassword(passwordEncoder.encode(clientRequest.password())); // Hashed Password
+        client.setRoleSet(Set.of(role));
 
-        client.setRoleSet(defaultRole);
+        return clientRepository.save(client);
     }
 
 }
