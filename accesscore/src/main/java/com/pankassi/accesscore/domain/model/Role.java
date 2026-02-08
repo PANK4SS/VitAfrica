008c353b9e8f06a7c39e0a294167b939b9e03db0
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-08 06:11:12 -0500

    Role model attributes added

--- accesscore/src/main/java/com/pankassi/accesscore/domain/model/Role.java
+++ accesscore/src/main/java/com/pankassi/accesscore/domain/model/Role.java
@@ -1,4 +1,22 @@
 package com.pankassi.accesscore.domain.model;
 
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsRequired
+@NoArgsRequired
+@Table(namme="Role")
+@Builder
 public class Role {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="roleId")
+    private Long roleId;
+
+    @Column(name="roleName", nullable=false)
+    private String roleName;
+
+    @Column(name="description", nullable=false)
+    private String description;
 }
