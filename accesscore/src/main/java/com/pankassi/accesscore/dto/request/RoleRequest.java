1ae6a4965fb2a1bff46f7b99bd031310594ccc41
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-08 12:01:24 -0500

    RoleRequest dto complete

--- accesscore/src/main/java/com/pankassi/accesscore/dto/request/RoleRequest.java
+++ accesscore/src/main/java/com/pankassi/accesscore/dto/request/RoleRequest.java
@@ -1,4 +1,11 @@
 package com.pankassi.accesscore.dto.request;
 
-public record RoleRequest() {
+import jakarta.validation.constraints.NotBlank;
+
+public record RoleRequest(
+        @NotBlank(message = "The Role Name is required")
+        String name,
+        @NotBlank(message = "The Role description is required")
+        String description
+) {
 }
