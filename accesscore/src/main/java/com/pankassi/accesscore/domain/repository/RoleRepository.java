4cb709185c602236e36f77c53e307870970c0981
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-08 12:50:07 -0500

    RoleRepository updated : now we can found Role by providing role name

--- accesscore/src/main/java/com/pankassi/accesscore/domain/repository/RoleRepository.java
+++ accesscore/src/main/java/com/pankassi/accesscore/domain/repository/RoleRepository.java
@@ -2,6 +2,9 @@ package com.pankassi.accesscore.domain.repository;
 import com.pankassi.accesscore.domain.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface RoleRepository extends JpaRepository<Role,Long>{
+    Optional<Role> findByRoleName(String roleName);
 }
