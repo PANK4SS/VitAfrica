2d1cb5dfd145f3b014a7421e2f2de938e3a75596
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-08 11:42:28 -0500

    RoleRepository imports fixed

--- accesscore/src/main/java/com/pankassi/accesscore/domain/repository/RoleRepository.java
+++ accesscore/src/main/java/com/pankassi/accesscore/domain/repository/RoleRepository.java
@@ -1,4 +1,7 @@
 package com.pankassi.accesscore.domain.repository;
+import com.pankassi.accesscore.domain.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 
 public interface RoleRepository extends JpaRepository<Role,Long>{
 }
