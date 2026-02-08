ef29c3b91f252e675311321bf5563f4411cd0e83
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-08 11:41:53 -0500

    ClientRepository import fixed

--- accesscore/src/main/java/com/pankassi/accesscore/domain/repository/ClientRepository.java
+++ accesscore/src/main/java/com/pankassi/accesscore/domain/repository/ClientRepository.java
@@ -1,4 +1,7 @@
 package com.pankassi.accesscore.domain.repository;
 
+import com.pankassi.accesscore.domain.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface ClientRepository extends JpaRepository<Client,Long> {
 }
