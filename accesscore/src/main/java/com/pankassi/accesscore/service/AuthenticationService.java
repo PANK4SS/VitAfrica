ab21ca2bc3187b4593702aab079d7ed1c453560f
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-08 12:46:48 -0500

    AuthenticationService: Feature registerClint init

--- accesscore/src/main/java/com/pankassi/accesscore/service/AuthenticationService.java
+++ accesscore/src/main/java/com/pankassi/accesscore/service/AuthenticationService.java
@@ -1,21 +1,41 @@
 package com.pankassi.accesscore.service;
 
+import com.pankassi.accesscore.domain.model.Client;
 import com.pankassi.accesscore.domain.repository.ClientRepository;
 import com.pankassi.accesscore.domain.repository.RoleRepository;
+import com.pankassi.accesscore.dto.request.ClientRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
     @Autowired
-    private ClientRepository clientRepository;
+    private final ClientRepository clientRepository;
 
     @Autowired
-    private RoleRepository roleRepository;
+    private final RoleRepository roleRepository;
+
+    private final PasswordEncoder passwordEncoder;
 
     @Value("${app.default.role:USER}")
     private String defaultRole;
+
+    public Client registerClient(ClientRequest clientRequest){
+        // Check if the user provide email already exist in DB
+        if(clientRepository.existByEmail(clientRequest.email())){
+            throw new IllegalStateException("The provided email is already used");
+        }
+        // At the moment that the provided email don't exist
+        Client client = new Client();
+        client.setClientName(clientRequest.username());
+        client.setEmail(clientRequest.email());
+        client.setPassword(passwordEncoder.encode(clientRequest.password())); // Hashed Password
+
+        client.setRoleSet(defaultRole);
+    }
+
 }
