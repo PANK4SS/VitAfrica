bc18056b3aba43aa93ccf230fd221043d1595201
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-08 06:04:56 -0500

    Client model attributes added; dependencies are managed boilerplates and gestter and setter automaticaly|

--- accesscore/src/main/java/com/pankassi/accesscore/domain/model/Client.java
+++ accesscore/src/main/java/com/pankassi/accesscore/domain/model/Client.java
@@ -1,4 +1,24 @@
 package com.pankassi.accesscore.domain.model;
 
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="Client")
 public class Client {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "client")
+    private Long clientId;
+
+    @Column(name="clientName",nullable=false,length=100)
+    private String clientName;
+
+    @Column(name="email",nullable=false)
+    private String emai;
+
+    @Column(name="password",nullable=false,length=16)
+    private String password;
 }
