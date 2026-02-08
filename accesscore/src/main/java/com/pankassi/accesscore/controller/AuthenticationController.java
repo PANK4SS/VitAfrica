63e46f8681798bb79d8886317c83d66ed8b84a5d
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-08 14:04:58 -0500

    feat(auth): add client registration endpoint

--- accesscore/src/main/java/com/pankassi/accesscore/controller/AuthenticationController.java
+++ accesscore/src/main/java/com/pankassi/accesscore/controller/AuthenticationController.java
@@ -1,4 +1,28 @@
 package com.pankassi.accesscore.controller;
 
+import com.pankassi.accesscore.domain.model.Client;
+import com.pankassi.accesscore.dto.request.ClientRequest;
+import com.pankassi.accesscore.dto.response.ClientResponse;
+import com.pankassi.accesscore.service.AuthenticationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/clients")
 public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<ClientResponse> registerClient(@Valid @RequestBody ClientRequest clientRequest){
+            Client client = authenticationService.registerClient(clientRequest);
+            return ResponseEntity.status(HttpStatus.CREATED).body(ClientResponse.from(client));
+    }
 }
