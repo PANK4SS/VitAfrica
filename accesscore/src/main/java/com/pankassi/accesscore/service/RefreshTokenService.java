12a334d55c0754680ebf2f65cc181c268aa7b5d7
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-09 05:48:19 -0500

    feat(accesscore): RefreshTokenService.java config and implemented

--- accesscore/src/main/java/com/pankassi/accesscore/service/RefreshTokenService.java
+++ accesscore/src/main/java/com/pankassi/accesscore/service/RefreshTokenService.java
@@ -1,4 +1,47 @@
 package com.pankassi.accesscore.service;
 
+import com.pankassi.accesscore.domain.model.Client;
+import com.pankassi.accesscore.domain.model.RefreshToken;
+import com.pankassi.accesscore.domain.repository.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Value("${app.jwt.refresh-expiration}")
+    private long refreshExpiration; // en millisecondes
+
+    public RefreshToken createRefreshToken(Client client) {
+        // Nettoyer les anciens tokens de cet utilisateur (optionnel, mais propre)
+        refreshTokenRepository.deleteByClientId(client.getClientId());
+
+        RefreshToken refreshToken = RefreshToken.builder()
+                .client(client)
+                .token(UUID.randomUUID().toString()) // Génération d'une chaîne aléatoire
+                .expiryDate(Instant.now().plusMillis(refreshExpiration))
+                .build();
+
+        return refreshTokenRepository.save(refreshToken);
+    }
+
+    public Optional<RefreshToken> findByToken(String token) {
+        return refreshTokenRepository.findByToken(token);
+    }
+
+    public RefreshToken verifyExpiration(RefreshToken token) {
+        if (token.getExpiryDate().compareTo(Instant.now()) < 0) {
+            refreshTokenRepository.delete(token);
+            throw new RuntimeException("Refresh token was expired. Please make a new signin request");
+        }
+        return token;
+    }
 }
