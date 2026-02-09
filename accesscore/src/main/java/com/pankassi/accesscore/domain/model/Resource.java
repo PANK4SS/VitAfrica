5af8c6d986a11dcfacd622de51eb3a250d604e2b
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-08 20:52:04 -0500

    feat(accesscore): initializing Resource.java model

--- accesscore/src/main/java/com/pankassi/accesscore/domain/model/Resource.java
+++ accesscore/src/main/java/com/pankassi/accesscore/domain/model/Resource.java
@@ -1,4 +1,28 @@
 package com.pankassi.accesscore.domain.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name="Resource")
 public class Resource {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="resourceId")
+    private Long resourceId;
+
+    @Column(name = "resourceName", nullable = false, unique = true)
+    @EqualsAndHashCode.Include
+    private String resourceName;
+
+    @Column(name = "resourceDescription")
+    private String resourceDescription;
+
+    
 }
