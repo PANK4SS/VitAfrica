e20e16c26ca8d78138c4f6f61c811fe16e345223
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-08 20:29:51 -0500

    fixed(accesscore): remover password sending fater assignRoles request in the body Response

--- accesscore/src/main/java/com/pankassi/accesscore/controller/ClientController.java
+++ accesscore/src/main/java/com/pankassi/accesscore/controller/ClientController.java
@@ -2,6 +2,7 @@ package com.pankassi.accesscore.controller;
 
 import com.pankassi.accesscore.domain.model.Client;
 import com.pankassi.accesscore.dto.request.AssignRolesRequest;
+import com.pankassi.accesscore.dto.response.ClientResponse;
 import com.pankassi.accesscore.service.ClientService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -16,11 +17,12 @@ public class ClientController {
 
     //Assign existing role to an existing client endpoint
     @PostMapping("/{email}/roles")
-    public ResponseEntity<Client> assignRoles(
-            @PathVariable String email,
-            @Valid @RequestBody AssignRolesRequest request
+    public ResponseEntity<ClientResponse> assignRoles(  // ← Changé de Client à ClientResponse
+                                                        @PathVariable String email,
+                                                        @Valid @RequestBody AssignRolesRequest request
     ) {
         Client updatedClient = clientService.assignRoles(email, request.roles());
-        return ResponseEntity.ok(updatedClient);
+        ClientResponse response = ClientResponse.from(updatedClient);  // ← Conversion en DTO
+        return ResponseEntity.ok(response);
     }
 }
