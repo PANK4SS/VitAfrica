3bc1b6f22b6dc1cac3ef7d35c3c39d2fa6a01461
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-08 21:15:59 -0500

    feat(accesscore): ResourceRepository rady for Resource existence checking and finding resource by resource name

--- accesscore/src/main/java/com/pankassi/accesscore/domain/repository/ResourceRepository.java
+++ accesscore/src/main/java/com/pankassi/accesscore/domain/repository/ResourceRepository.java
@@ -1,4 +1,12 @@
 package com.pankassi.accesscore.domain.repository;
 
-public interface ResourceRepository {
+import com.pankassi.accesscore.domain.model.Resource;
+import com.pankassi.accesscore.domain.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ResourceRepository extends JpaRepository<Resource,Long> {
+    Optional<Role> findByResourceName(String resourceName);
+    boolean existsByResourceName(String resourceName);
 }
