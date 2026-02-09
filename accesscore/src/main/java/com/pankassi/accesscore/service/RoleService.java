2db965f9eed95dc2e1241b2db97d730faf22773a
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-09 03:43:00 -0500

    chore(RoleService): unused imports removed from RoleService

--- accesscore/src/main/java/com/pankassi/accesscore/service/RoleService.java
+++ accesscore/src/main/java/com/pankassi/accesscore/service/RoleService.java
@@ -1,16 +1,25 @@
 package com.pankassi.accesscore.service;
 
+
+import com.pankassi.accesscore.domain.model.Resource;
 import com.pankassi.accesscore.domain.model.Role;
+import com.pankassi.accesscore.domain.repository.ResourceRepository;
 import com.pankassi.accesscore.domain.repository.RoleRepository;
+import com.pankassi.accesscore.dto.request.AssignResourcesRequest;
 import com.pankassi.accesscore.dto.request.RoleRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.HashSet;
+import java.util.Set;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RoleService {
     private final RoleRepository roleRepository;
+    private final ResourceRepository resourceRepository;
 
     public Role createRole(RoleRequest roleRequest){
         // Check if provide role Name already exist
@@ -23,6 +32,42 @@ public class RoleService {
         Role role = new Role();
         role.setRoleName(roleName);
         role.setDescription(roleRequest.description());
+
+        return roleRepository.save(role);
+    }
+
+    @Transactional
+    public Role assignResources(AssignResourcesRequest request) {
+        //Role existence checking and collection
+        Role role = roleRepository.findByRoleName(request.roleName().toUpperCase())
+                .orElseThrow(() ->
+                        new IllegalArgumentException("Role not found: " + request.roleName())
+                );
+
+        log.info("Role found: {} with existing resources: {}",
+                role.getRoleName(),
+                role.getResourceSet().stream()
+                        .map(Resource::getResourceName)
+                        .toList());
+
+
+        // Resource Collection
+        Set<Resource> newResources = new HashSet<>();
+        for (String resourceName : request.resources()) {
+            Resource resource = resourceRepository.findByResourceName(resourceName)
+                    .orElseThrow(() ->
+                            new IllegalArgumentException("Resource not found: " + resourceName)
+                    );
+            newResources.add(resource);
+        }
+
+        // Assignment
+        role.getResourceSet().addAll(newResources);
+
+        log.info("After adding resources: {}",
+                role.getResourceSet().stream()
+                        .map(Resource::getResourceName)
+                        .toList());
 
         return roleRepository.save(role);
     }
