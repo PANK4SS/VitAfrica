c1b90c9cabc2db6a31b25330023a46dc3d73b08f
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-09 06:31:45 -0500

    feat(accesscore): RefreshTokenRepository.java completly implement

--- accesscore/src/main/java/com/pankassi/accesscore/domain/repository/RefreshTokenRepository.java
+++ accesscore/src/main/java/com/pankassi/accesscore/domain/repository/RefreshTokenRepository.java
@@ -3,15 +3,18 @@ package com.pankassi.accesscore.domain.repository;
 import com.pankassi.accesscore.domain.model.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken,Long> {
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByToken(String token);
 
-    // Delete all tokens from a client (useful when reconnecting to prevent proliferation)
+    // CORRECTION ICI :
+    // Cela signifie : "Trouve le RefreshToken où son 'client' a pour 'clientId' la valeur donnée"
     @Modifying
     @Transactional
-    void deleteByClientId(Long clientId);
+    void deleteByClientClientId(Long clientId);
 }
