3cde759c5c3f8db11bfb66b3a4efddcbbba54da1
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-08 22:41:09 -0500

    feat(accesscore): Resource model updated to store HttpMethod and also endpoints

--- accesscore/src/main/java/com/pankassi/accesscore/domain/model/Resource.java
+++ accesscore/src/main/java/com/pankassi/accesscore/domain/model/Resource.java
@@ -20,12 +20,22 @@ public class Resource {
     @Column(name="resourceId")
     private Long resourceId;
 
+    // The technical code (e.g., "BOOK_CREATE")
+    // This is what you will use in @PreAuthorize("hasAuthority('BOOK_CREATE')")
     @Column(name = "resourceName", nullable = false, unique = true)
     @EqualsAndHashCode.Include
     private String resourceName;
 
     @Column(name = "resourceDescription")
     private String resourceDescription;
+
+    // The corresponding HTTP method
+    @Column(name = "httpMethod", nullable = false)
+    private String httpMethod; // "GET", "POST", "PUT", "DELETE"
+
+    // The endpoint path (Ant pattern)
+    @Column(name = "endpoint", nullable = false)
+    private String endpoint; // "/api/books"
 
     @ManyToMany(mappedBy="resourceSet")
     @JsonManagedReference
