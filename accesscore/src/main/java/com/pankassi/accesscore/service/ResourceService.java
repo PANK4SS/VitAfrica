26a1f701f37abf4ec57692bb866cac3bd6d3ead3
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-08 22:16:21 -0500

    feat(accesscore): ResourceService base service  code initalize

--- accesscore/src/main/java/com/pankassi/accesscore/service/ResourceService.java
+++ accesscore/src/main/java/com/pankassi/accesscore/service/ResourceService.java
@@ -1,4 +1,30 @@
 package com.pankassi.accesscore.service;
 
+import com.pankassi.accesscore.domain.model.Resource;
+import com.pankassi.accesscore.domain.model.Role;
+import com.pankassi.accesscore.domain.repository.ResourceRepository;
+import com.pankassi.accesscore.dto.request.ResourceRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ResourceService {
+    private final ResourceRepository resourceRepository;
+
+    public Resource createResource(ResourceRequest resourceRequest){
+        // Check if provide resource Name already exist
+        if(resourceRepository.existsByResourceName(resourceRequest.name())){
+            throw new IllegalStateException("The provided Resource Already exists");
+        }
+
+        String resourceName = resourceRequest.name().trim().toUpperCase();
+
+        Resource resource = new Resource();
+        resource.setResourceName(resourceName);
+        resource.setResourceDescription(resourceRequest.description());
+
+        return resourceRepository.save(resource);
+    }
+
 }
