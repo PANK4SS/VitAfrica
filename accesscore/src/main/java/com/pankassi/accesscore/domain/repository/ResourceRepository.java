fb64c622beeb40e6225d6696e87d4bf927cee171
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-09 03:41:14 -0500

    fix(accesscore): findByResourceName type fixed from Optional<Role> to Optional<Resource>

--- accesscore/src/main/java/com/pankassi/accesscore/domain/repository/ResourceRepository.java
+++ accesscore/src/main/java/com/pankassi/accesscore/domain/repository/ResourceRepository.java
@@ -1,12 +1,11 @@
 package com.pankassi.accesscore.domain.repository;
 
 import com.pankassi.accesscore.domain.model.Resource;
-import com.pankassi.accesscore.domain.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface ResourceRepository extends JpaRepository<Resource,Long> {
-    Optional<Role> findByResourceName(String resourceName);
+    Optional<Resource> findByResourceName(String resourceName);
     boolean existsByResourceName(String resourceName);
 }
