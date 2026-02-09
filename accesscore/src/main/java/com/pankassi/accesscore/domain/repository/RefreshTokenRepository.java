e3e2726d4c8306d8913232bb3b49c8319598b2f8
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-09 05:44:11 -0500

    feat(accesscore): RefreshTokenRepository config and implement

--- accesscore/src/main/java/com/pankassi/accesscore/domain/repository/RefreshTokenRepository.java
+++ accesscore/src/main/java/com/pankassi/accesscore/domain/repository/RefreshTokenRepository.java
@@ -1,4 +1,17 @@
 package com.pankassi.accesscore.domain.repository;
 
-public interface RefreshTokenRepository {
+import com.pankassi.accesscore.domain.model.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken,Long> {
+    Optional<RefreshToken> findByToken(String token);
+
+    // Delete all tokens from a client (useful when reconnecting to prevent proliferation)
+    @Modifying
+    @Transactional
+    void deleteByClientId(Long clientId);
 }
