e30f5d2ca06d08c7e362d447a66f3c74ce6a9503
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-08 22:42:28 -0500

    feat(accesscore): ResourceResponse updated to match with the model new form

--- accesscore/src/main/java/com/pankassi/accesscore/dto/response/ResourceResponse.java
+++ accesscore/src/main/java/com/pankassi/accesscore/dto/response/ResourceResponse.java
@@ -6,14 +6,18 @@ import com.pankassi.accesscore.domain.model.Resource;
 public record ResourceResponse(
         Long resourceId,
         String resourceName,
-        String resourceDescription
+        String resourceDescription,
+        String HttpMethod,
+        String endpoint
 ) {
     //Convert resource into RoleResponse
     public static ResourceResponse from(Resource resource){
         return new ResourceResponse(
                 resource.getResourceId(),
                 resource.getResourceName(),
-                resource.getResourceDescription()
+                resource.getResourceDescription(),
+                resource.getHttpMethod(),
+                resource.getEndpoint()
         );
     }
 }
