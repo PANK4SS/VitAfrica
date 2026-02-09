c6a5a7092a6d6c9bffa2725a44b4b43e806e10a0
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-09 04:15:54 -0500

    feat(accesscoe): LoginRequestt dto implement for data maping after login

--- accesscore/src/main/java/com/pankassi/accesscore/dto/request/LoginRequest.java
+++ accesscore/src/main/java/com/pankassi/accesscore/dto/request/LoginRequest.java
@@ -1,4 +1,14 @@
 package com.pankassi.accesscore.dto.request;
 
-public record LoginRequest() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+        @NotBlank(message = "The Email is required")
+        @Email(message = "The provided Email should be a valid one")
+        String email,
+
+        @NotBlank(message = "The Password is required")
+        String password
+) {
 }
