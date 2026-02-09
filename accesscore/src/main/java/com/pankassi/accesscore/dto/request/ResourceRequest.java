ee822943c03de7cf323a9c8e855e47737bd9ea69
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-08 22:42:02 -0500

    feat(accesscore): ResourceRequest updated to match with the model new form

--- accesscore/src/main/java/com/pankassi/accesscore/dto/request/ResourceRequest.java
+++ accesscore/src/main/java/com/pankassi/accesscore/dto/request/ResourceRequest.java
@@ -6,6 +6,10 @@ public record ResourceRequest(
         @NotBlank(message = "The Resource Name is required")
         String name,
         @NotBlank(message = "The Resource description is required")
-        String description
+        String description,
+        @NotBlank(message = "The HTTP Method is required")
+        String httpMethod,
+        @NotBlank(message = "The endpoint is required")
+        String endpoint
 ) {
 }
