370d478c18355a3bba010b495ac4690f0aad44c9
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-09 04:14:15 -0500

    feat(accesscore): RefreshToken model, LoginRequest dto and AuthenticationResponse dto init

---
+++ accesscore/src/main/java/com/pankassi/accesscore/domain/model/RefreshToken.java
@@ -0,0 +1,28 @@
+package com.pankassi.accesscore.domain.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "refresh_token")
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
+    private Instant expiryDate;
+}
