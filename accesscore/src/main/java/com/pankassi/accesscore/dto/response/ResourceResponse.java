29e3256e96adc5b5f694c428a32aabff88e1d092
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-08 22:15:50 -0500

    feat(accesscore): ResourceService base service  code initalize

--- accesscore/src/main/java/com/pankassi/accesscore/dto/response/ResourceResponse.java
+++ accesscore/src/main/java/com/pankassi/accesscore/dto/response/ResourceResponse.java
@@ -1,4 +1,19 @@
 package com.pankassi.accesscore.dto.response;
 
-public record ResourceResponse() {
+
+import com.pankassi.accesscore.domain.model.Resource;
+
+public record ResourceResponse(
+        Long resourceId,
+        String resourceName,
+        String resourceDescription
+) {
+    //Convert resource into RoleResponse
+    public static ResourceResponse from(Resource resource){
+        return new ResourceResponse(
+                resource.getResourceId(),
+                resource.getResourceName(),
+                resource.getResourceDescription()
+        );
+    }
 }
