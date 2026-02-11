c67eb8fc8fdd4fc84d53d08df4185430b1723974
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-11 05:28:25 -0500

    feat(backend): backend main entry point class updated to scan entiies in com.pankassi.backend.domain.model and com.pankassi.accesscore.domain.model

--- backend/src/main/java/com/pankassi/backend/BackendApplication.java
+++ backend/src/main/java/com/pankassi/backend/BackendApplication.java
@@ -2,8 +2,14 @@ package com.pankassi.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+// We tell Hibernate to scan entities IN both projects:
+@EntityScan(basePackages = {
+		"com.pankassi.backend.domain.model", // Our entities (Patient)
+		"com.pankassi.accesscore.domain.model" // The entities in the library (Client)
+})
 public class BackendApplication {
 
 	public static void main(String[] args) {
