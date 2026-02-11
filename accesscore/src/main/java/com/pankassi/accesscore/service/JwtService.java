48e419758aac96ca9d15d38e17fb4e5f53ddc504
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-11 12:10:22 -0500

    fix(accesscore): JwtService updatedd

--- accesscore/src/main/java/com/pankassi/accesscore/service/JwtService.java
+++ accesscore/src/main/java/com/pankassi/accesscore/service/JwtService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.List; 
 import java.util.Set;
 import java.util.stream.Collectors;
 
