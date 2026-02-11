044981c3977bfd70fdb1acaba157bb8b5e811e39
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-11 13:06:33 -0500

    feat(backend): Doctor model completly init and linked to Appointment

--- backend/src/main/java/com/pankassi/backend/domain/model/Doctor.java
+++ backend/src/main/java/com/pankassi/backend/domain/model/Doctor.java
@@ -1,4 +1,39 @@
 package com.pankassi.backend.domain.model;
 
+import com.pankassi.accesscore.domain.model.Client;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "doctor")
 public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "doctor_id")
+    private Long doctorId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String department;
+
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "clientId", unique = true, nullable = false)
+    private Client client;
+
+
+    @OneToMany(mappedBy = "doctor", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<Appointment> appointments = new ArrayList<>();
 }
