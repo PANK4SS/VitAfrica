dd50250717e6d555b49b7c679868693d07f8e94c
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-11 12:15:00 -0500

    feat(backend): VitalSigns initializing'

--- backend/src/main/java/com/pankassi/backend/domain/model/VitalSigns.java
+++ backend/src/main/java/com/pankassi/backend/domain/model/VitalSigns.java
@@ -1,4 +1,18 @@
 package com.pankassi.backend.domain.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "vitalSigns")
 public class VitalSigns {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "vitalSignId")
+    private Long vitalSignId;
 }
