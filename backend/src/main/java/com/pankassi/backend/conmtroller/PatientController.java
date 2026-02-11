983a5db838018ca7a5821ade85c725ce36f7a1a4
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-10 19:27:05 -0500

    feat(backend): controller directory include in the architecture

---
+++ backend/src/main/java/com/pankassi/backend/conmtroller/PatientController.java
@@ -0,0 +1,11 @@
+package com.pankassi.backend.conmtroller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/patients")
+@RequiredArgsConstructor
+public class PatientController {
+}
