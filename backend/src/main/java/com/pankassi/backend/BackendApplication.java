203d76789630cb545668ccac8e36cf3bd7b1c307
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-11 05:57:00 -0500

    feat(backend): BackendApplication updated to include all required components for accesscore libry scanning

--- backend/src/main/java/com/pankassi/backend/BackendApplication.java
+++ backend/src/main/java/com/pankassi/backend/BackendApplication.java
@@ -3,12 +3,18 @@ package com.pankassi.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 // We tell Hibernate to scan entities IN both projects:
 @EntityScan(basePackages = {
 		"com.pankassi.backend.domain.model", // Our entities (Patient)
 		"com.pankassi.accesscore.domain.model" // The entities in the library (Client)
+})
+
+@ComponentScan(basePackages = {
+		"com.pankassi.backend",              // Components (PatientService, PatientController)
+		"com.pankassi.accesscore"           // Library components(AuthenticationService, SecurityConfig, etc.)
 })
 public class BackendApplication {
 
