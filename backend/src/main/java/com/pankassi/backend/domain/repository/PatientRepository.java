a607de67cdde6f8bc6ad0581af7bebd607c04dc9
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-11 13:40:37 -0500

    feat(backend): PatientRepository implemented

--- backend/src/main/java/com/pankassi/backend/domain/repository/PatientRepository.java
+++ backend/src/main/java/com/pankassi/backend/domain/repository/PatientRepository.java
@@ -1,7 +1,11 @@
 package com.pankassi.backend.domain.repository;
 
+import com.pankassi.accesscore.domain.model.Client;
 import com.pankassi.backend.domain.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Optional<Patient> findByClient(Client client);
 }
