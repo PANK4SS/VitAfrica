df219cba1724ce0105bea738b247657a34c7c8ad
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-10 19:29:26 -0500

    feat(backend): service directory added to the backend architecture

---
+++ backend/src/main/java/com/pankassi/backend/service/PatientService.java
@@ -0,0 +1,41 @@
+package com.pankassi.backend.service;
+
+import com.pankassi.accesscore.domain.model.Client;
+import com.pankassi.accesscore.dto.request.ClientRequest;
+import com.pankassi.accesscore.dto.response.ClientResponse;
+import com.pankassi.accesscore.service.AuthenticationService;
+import com.pankassi.backend.domain.model.Patient;
+import com.pankassi.backend.domain.repository.PatientRepository;
+import com.pankassi.backend.dto.request.RegisterPatientRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PatientService {
+    private final PatientRepository patientRepository;
+    private final AuthenticationService authenticationService; // Comes from AccessCore
+
+    //Patient Registration
+    public ClientResponse registerPatient (RegisterPatientRequest registerPatientRequest){
+        // Manage registration using AccessCore
+        ClientRequest clientRequest = new ClientRequest(
+                registerPatientRequest.email(),
+                registerPatientRequest.username(),
+                registerPatientRequest.password());
+
+        Client newClient = authenticationService.registerClient(clientRequest);
+
+        // Save data about phone, locationAdress and profilePic
+        Patient patient = Patient.builder()
+                .phone(registerPatientRequest.phone())
+                .locationAddress(registerPatientRequest.locationAddress())
+                .profilePicUrl(registerPatientRequest.profilePicUrl())
+                        .client(newClient).build();
+
+        patientRepository.save(patient);
+
+        return ClientResponse.from(newClient);
+    }
+
+}
