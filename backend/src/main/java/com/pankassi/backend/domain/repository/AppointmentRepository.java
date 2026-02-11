f0cb6e57c55a89df3aa5f43f1e9a474c1a01970e
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-11 13:40:00 -0500

    feat(backend): AppointmentRepository init to share data about appointment

--- backend/src/main/java/com/pankassi/backend/domain/repository/AppointmentRepository.java
+++ backend/src/main/java/com/pankassi/backend/domain/repository/AppointmentRepository.java
@@ -1,4 +1,15 @@
 package com.pankassi.backend.domain.repository;
 
-public interface AppointmentRepository {
+import com.pankassi.backend.domain.model.Appointment;
+import com.pankassi.backend.domain.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+    // Find last patient appointment filterd by date Desc
+    Optional<Appointment> findFirstByPatientOrderByDateTimeDesc(Patient patient);
 }
