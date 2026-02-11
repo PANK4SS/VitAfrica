106cc4201c55a2d413788b7aae14cbdc08a6875e
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-11 05:03:43 -0500

    feat(backend): PatientService updated to implement the specifi custom role for registration

--- backend/src/main/java/com/pankassi/backend/service/PatientService.java
+++ backend/src/main/java/com/pankassi/backend/service/PatientService.java
@@ -17,21 +17,21 @@ public class PatientService {
     private final AuthenticationService authenticationService; // Comes from AccessCore
 
     //Patient Registration
-    public ClientResponse registerPatient (RegisterPatientRequest registerPatientRequest){
+    public ClientResponse registerPatient (RegisterPatientRequest registerPatientRequest,String roleName){
         // Manage registration using AccessCore
         ClientRequest clientRequest = new ClientRequest(
                 registerPatientRequest.email(),
                 registerPatientRequest.username(),
                 registerPatientRequest.password());
 
-        Client newClient = authenticationService.registerClient(clientRequest);
+        Client newClient = authenticationService.registerClient(clientRequest,roleName);
 
         // Save data about phone, locationAdress and profilePic
         Patient patient = Patient.builder()
                 .phone(registerPatientRequest.phone())
                 .locationAddress(registerPatientRequest.locationAddress())
                 .profilePicUrl(registerPatientRequest.profilePicUrl())
-                        .client(newClient).build();
+                .client(newClient).build();
 
         patientRepository.save(patient);
 
