43f084312459dae8ae6b39f65383ff594deb07b1
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-11 12:33:15 -0500

    feat(accesscore): VitalSigns models completly initialize

--- backend/src/main/java/com/pankassi/backend/domain/model/VitalSigns.java
+++ backend/src/main/java/com/pankassi/backend/domain/model/VitalSigns.java
@@ -3,6 +3,8 @@ package com.pankassi.backend.domain.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @Setter
@@ -11,8 +13,28 @@ import lombok.*;
 @NoArgsConstructor
 @Table(name = "vitalSigns")
 public class VitalSigns {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "vitalSignId")
     private Long vitalSignId;
+
+    @Column(name = "bloodPressure")
+    private String bloodPressure;
+
+    @Column(name = "heart_rate")
+    private Integer heartRate;
+
+    @Column(name = "temperature")
+    private Double temperature;
+
+    @Column(name = "weight")
+    private Double weight;
+
+    @Column(name = "date_measured", nullable = false)
+    private LocalDateTime dateMeasured;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patientId", nullable = false)
+    private Patient patient;
 }
