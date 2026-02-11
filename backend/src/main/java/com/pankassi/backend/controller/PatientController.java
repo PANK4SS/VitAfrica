9baed12a37f78af1a164deff0a5ecd193f4f8e39
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-10 19:28:41 -0500

    fix(backend): controller directory name updated

---
+++ backend/src/main/java/com/pankassi/backend/controller/PatientController.java
@@ -0,0 +1,11 @@
+package com.pankassi.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/patients")
+@RequiredArgsConstructor
+public class PatientController {
+}
