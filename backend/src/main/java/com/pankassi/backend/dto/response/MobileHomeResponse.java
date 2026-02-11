fc27395675f657dcabe59b4d958fa65ac3f7f0d9
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-11 13:20:12 -0500

    feat(backend): MobileHomeResponse dto completly init

--- backend/src/main/java/com/pankassi/backend/dto/response/MobileHomeResponse.java
+++ backend/src/main/java/com/pankassi/backend/dto/response/MobileHomeResponse.java
@@ -1,4 +1,15 @@
 package com.pankassi.backend.dto.response;
 
-public record MobileHomeResponse() {
-}
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record MobileHomeResponse(
+        String userName,
+        String profilePic,
+        String date,   // The appointment date
+        String hour,   // The appointment hour
+        String appointmentStatus,
+        String doctorName,
+        String doctorDepartment,
+        List<String> vitalSignsConstants
+) {}
