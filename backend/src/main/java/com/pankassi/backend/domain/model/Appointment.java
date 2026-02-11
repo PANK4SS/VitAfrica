ef788b784c9ce1c0295324417dca68fcf91634c7
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-11 13:05:59 -0500

    feat(backend): Appointment completly init

--- backend/src/main/java/com/pankassi/backend/domain/model/Appointment.java
+++ backend/src/main/java/com/pankassi/backend/domain/model/Appointment.java
@@ -1,4 +1,35 @@
 package com.pankassi.backend.domain.model;
 
+import com.pankassi.backend.domain.model.Doctor;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "appointment")
 public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "appointment_id")
+    private Long appointmentId;
+
+    @Column(name = "date_time", nullable = false)
+    private LocalDateTime dateTime;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id", nullable = false)
+    private Patient patient;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "doctor_id", nullable = false)
+    private Doctor doctor;
 }
