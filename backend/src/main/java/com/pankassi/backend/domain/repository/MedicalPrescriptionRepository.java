fdbf86de713887f910d9203c65f84a39bc525732
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-12 08:10:45 -0500

    feat(backend): MedicalPrscriptionRepository initalize to return Medical Prescription

--- backend/src/main/java/com/pankassi/backend/domain/repository/MedicalPrescriptionRepository.java
+++ backend/src/main/java/com/pankassi/backend/domain/repository/MedicalPrescriptionRepository.java
@@ -1,4 +1,13 @@
 package com.pankassi.backend.domain.repository;
 
-public interface MedicalPrescriptionRepository {
+import com.pankassi.backend.domain.model.MedicalPrescription;
+import com.pankassi.backend.domain.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MedicalPrescriptionRepository extends JpaRepository<MedicalPrescription,Long> {
+    List<MedicalPrescription> findByPatientOrderByPrescriptionDateDesc(Patient patient);
 }
