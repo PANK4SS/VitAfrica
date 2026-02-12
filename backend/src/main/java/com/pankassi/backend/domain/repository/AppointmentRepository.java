6bf84bc004dee776c8f8e72508f24be7fae28e34
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-11 21:32:12 -0500

    feat(backend): AppointmentRepository updated for appointment transfering management

--- backend/src/main/java/com/pankassi/backend/domain/repository/AppointmentRepository.java
+++ backend/src/main/java/com/pankassi/backend/domain/repository/AppointmentRepository.java
@@ -5,10 +5,14 @@ import com.pankassi.backend.domain.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
     // Find last patient appointment filtered by date Desc and status confirmed
     Optional<Appointment> findFirstByPatientAndStatusOrderByDateTimeDesc(Patient patient, String status);
+
+
+    List<Appointment> findByPatientAndStatusInOrderByDateTimeDesc(Patient patient, List<String> statuses);
 }
