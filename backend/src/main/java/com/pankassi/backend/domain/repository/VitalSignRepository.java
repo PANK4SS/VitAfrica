eea37521d0ff770a1158b5750fc475df20585e5b
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-11 21:18:56 -0500

    feat(backend): VitalRepository completed for mobile App Home page requirements

--- backend/src/main/java/com/pankassi/backend/domain/repository/VitalSignRepository.java
+++ backend/src/main/java/com/pankassi/backend/domain/repository/VitalSignRepository.java
@@ -1,7 +1,13 @@
 package com.pankassi.backend.domain.repository;
 
 import com.pankassi.backend.domain.model.VitalSign;
+import com.pankassi.backend.domain.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface VitalSignRepository extends JpaRepository<VitalSign,Long> {
+import java.util.Optional;
+
+@Repository
+public interface VitalSignRepository extends JpaRepository<VitalSign, Long> {
+    Optional<VitalSign> findFirstByPatientOrderByDateMeasuredDesc(Patient patient);
 }
