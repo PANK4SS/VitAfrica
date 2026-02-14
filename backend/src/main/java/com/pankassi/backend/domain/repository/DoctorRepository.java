76f752b146c6e647f1858f3f2d8cd01b5903dd50
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-14 18:30:35 -0500

    fix(backend): DoctorRepository init to collect doctor by Client

--- backend/src/main/java/com/pankassi/backend/domain/repository/DoctorRepository.java
+++ backend/src/main/java/com/pankassi/backend/domain/repository/DoctorRepository.java
@@ -1,4 +1,12 @@
 package com.pankassi.backend.domain.repository;
 
-public interface DoctorRepository {
+import com.pankassi.accesscore.domain.model.Client;
+import com.pankassi.backend.domain.model.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface DoctorRepository extends JpaRepository<Doctor,Long> {
+
+    Optional<Doctor> findByClient(Client client);
 }
