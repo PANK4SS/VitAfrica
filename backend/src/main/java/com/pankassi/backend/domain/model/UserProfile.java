fe690d3264b59396d839553bf07816e1dc6c8f6f
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-15 06:45:10 -0500

    feat(backend): UserProfile model initialization complete

--- backend/src/main/java/com/pankassi/backend/domain/model/UserProfile.java
+++ backend/src/main/java/com/pankassi/backend/domain/model/UserProfile.java
@@ -1,4 +1,29 @@
 package com.pankassi.backend.domain.model;
 
+import com.pankassi.accesscore.domain.model.Client;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user_profile")
 public class UserProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "profile_id")
+    private Long profileId;
+
+    @Column(name= "profile_pic_url", nullable = false)
+    private String profilePicUrl;
+
+    @Column(name="status",unique = true)
+    private String status;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "clientId", unique = true, nullable = false)
+    private Client client;
 }
