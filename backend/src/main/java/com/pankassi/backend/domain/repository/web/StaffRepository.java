327784302ff8dd9a9fc0edf6188e5a7cb014deec
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-15 06:47:40 -0500

    feat(backend): StaffRepository init

--- backend/src/main/java/com/pankassi/backend/domain/repository/web/StaffRepository.java
+++ backend/src/main/java/com/pankassi/backend/domain/repository/web/StaffRepository.java
@@ -1,4 +1,7 @@
 package com.pankassi.backend.domain.repository.web;
 
-public interface StaffRepository {
+import com.pankassi.backend.domain.model.Staff;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StaffRepository extends JpaRepository<Staff,Long> {
 }
