7756b035aa20427a53261aafc9f1cc29cc51f90f
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-15 12:55:33 -0500

    feat(backend): StaffRepository implemented for data inetraction

--- backend/src/main/java/com/pankassi/backend/domain/repository/web/StaffRepository.java
+++ backend/src/main/java/com/pankassi/backend/domain/repository/web/StaffRepository.java
@@ -1,7 +1,11 @@
 package com.pankassi.backend.domain.repository.web;
 
+import com.pankassi.accesscore.domain.model.Client;
 import com.pankassi.backend.domain.model.Staff;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StaffRepository extends JpaRepository<Staff,Long> {
+    Optional<Staff> findByClient(Client client);
 }
