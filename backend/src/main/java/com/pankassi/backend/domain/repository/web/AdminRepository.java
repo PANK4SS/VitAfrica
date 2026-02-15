8731ba753b60cd0690b0a7cb6193b8b381bed860
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-15 12:54:50 -0500

    feat(backend): AdminRepository implement for admin data inetraction

--- backend/src/main/java/com/pankassi/backend/domain/repository/web/AdminRepository.java
+++ backend/src/main/java/com/pankassi/backend/domain/repository/web/AdminRepository.java
@@ -1,7 +1,11 @@
 package com.pankassi.backend.domain.repository.web;
 
+import com.pankassi.accesscore.domain.model.Client;
 import com.pankassi.backend.domain.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AdminRepository extends JpaRepository<Admin,Long> {
+    Optional<Admin> findByClient(Client client);
 }
