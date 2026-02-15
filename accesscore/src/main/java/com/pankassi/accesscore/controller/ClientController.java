2dcc7179903cfa0bd942a3716d33de32f6cfe6fb
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-15 16:47:07 -0500

    fix(accesscore): ClientController endpoint name fixed

--- accesscore/src/main/java/com/pankassi/accesscore/controller/ClientController.java
+++ accesscore/src/main/java/com/pankassi/accesscore/controller/ClientController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/clients")
+@RequestMapping("api/clients")
 @RequiredArgsConstructor
 public class ClientController {
     private final ClientService clientService;
