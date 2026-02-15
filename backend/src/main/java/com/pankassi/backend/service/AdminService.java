c3ae8112a49e4665df84d797bf3d3e3d6bff77d4
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-15 06:48:33 -0500

    feat(backend): AdminService  init

--- backend/src/main/java/com/pankassi/backend/service/AdminService.java
+++ backend/src/main/java/com/pankassi/backend/service/AdminService.java
@@ -1,4 +1,10 @@
 package com.pankassi.backend.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AdminService {
+    private final
 }
