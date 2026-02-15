af07864b86d8d5e8696d787b94a1250c65815d92
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-15 12:50:30 -0500

    feat(backend): AdminController implement for dashboard stats rendering

--- backend/src/main/java/com/pankassi/backend/controller/web/Admin/AdminController.java
+++ backend/src/main/java/com/pankassi/backend/controller/web/Admin/AdminController.java
@@ -1,4 +1,27 @@
 package com.pankassi.backend.controller.web.Admin;
 
+import com.pankassi.backend.dto.response.Admin.AdminDashboardResponse;
+import com.pankassi.backend.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
+    private final AdminService adminService;
+
+    /**
+     * Endpoint for dashboard metrics collecting.
+     */
+    @GetMapping("/stats")
+    public ResponseEntity<AdminDashboardResponse> getDashboardStats() {
+        AdminDashboardResponse stats = adminService.usersStats();
+        return ResponseEntity.ok(stats);
+    }
 }
