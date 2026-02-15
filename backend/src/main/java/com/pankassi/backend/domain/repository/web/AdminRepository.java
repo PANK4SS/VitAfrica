45e53940d80780db6c28eb7b6c7d57c1e7d583bc
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-15 06:49:08 -0500

    fix(backend): AdminRepository class name fixed

--- backend/src/main/java/com/pankassi/backend/domain/repository/web/AdminRepository.java
+++ backend/src/main/java/com/pankassi/backend/domain/repository/web/AdminRepository.java
@@ -3,5 +3,5 @@ package com.pankassi.backend.domain.repository.web;
 import com.pankassi.backend.domain.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdminReposittory extends JpaRepository<Admin,Long> {
+public interface AdminRepository extends JpaRepository<Admin,Long> {
 }
