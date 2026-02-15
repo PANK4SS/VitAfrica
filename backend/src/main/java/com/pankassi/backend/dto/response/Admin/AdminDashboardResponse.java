d758b19470334ebbca49215e75f78c936ef8b66b
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-15 06:48:08 -0500

    feat(backend): AdminDashboardResponse dto complete

--- backend/src/main/java/com/pankassi/backend/dto/response/Admin/AdminDashboardResponse.java
+++ backend/src/main/java/com/pankassi/backend/dto/response/Admin/AdminDashboardResponse.java
@@ -1,4 +1,9 @@
 package com.pankassi.backend.dto.response.Admin;
 
-public record AdminDashboardResponse() {
+public record AdminDashboardResponse(
+        Integer patientNumber,
+        Integer doctorNumber,
+        Integer staffNumber,
+        Integer waitingRequestNumber
+) {
 }
