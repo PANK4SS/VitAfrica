dd103c6954db722e8f20764d11950d15aeba6bf9
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-15 06:44:31 -0500

    feat(backend): Staff model initalization complete

--- backend/src/main/java/com/pankassi/backend/domain/model/Staff.java
+++ backend/src/main/java/com/pankassi/backend/domain/model/Staff.java
@@ -1,4 +1,23 @@
 package com.pankassi.backend.domain.model;
 
+import com.pankassi.accesscore.domain.model.Client;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "staff")
 public class Staff {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "staff_id")
+    private Long staffId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "clientId", unique = true, nullable = false)
+    private Client client;
 }
