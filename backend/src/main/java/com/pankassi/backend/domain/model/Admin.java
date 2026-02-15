a28d592e447f10e0153d7cfa64c98879e65575ae
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-15 06:44:02 -0500

    feat(backend): Admin model initalization complete

--- backend/src/main/java/com/pankassi/backend/domain/model/Admin.java
+++ backend/src/main/java/com/pankassi/backend/domain/model/Admin.java
@@ -1,4 +1,23 @@
 package com.pankassi.backend.domain.model;
 
+import com.pankassi.accesscore.domain.model.Client;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "admin")
 public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "admin_id")
+    private Long adminId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "clientId", unique = true, nullable = false)
+    private Client client;
 }
