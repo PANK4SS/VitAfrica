7f714713aba6d9f794a1cd1f7b8bdc6bad9ea4f3
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-15 07:02:58 -0500

    feat(backend): AdminService initializing

--- backend/src/main/java/com/pankassi/backend/service/AdminService.java
+++ backend/src/main/java/com/pankassi/backend/service/AdminService.java
@@ -1,10 +1,19 @@
 package com.pankassi.backend.service;
 
+import com.pankassi.backend.domain.repository.DoctorRepository;
+import com.pankassi.backend.domain.repository.PatientRepository;
+import com.pankassi.backend.domain.repository.web.StaffRepository;
+import com.pankassi.backend.domain.repository.web.UserProfileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AdminService {
-    private final
+    private final PatientRepository patientRepository;
+    private final DoctorRepository doctorRepository;
+    private final StaffRepository staffRepository;
+    private  final UserProfileRepository userProfileRepository;
+
+    
 }
