7f1ee130b3f8b1731e2ad79bce68376c2a74d858
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-15 12:51:08 -0500

    feat(backend): WebAuthenticationController init for web registration and login

--- backend/src/main/java/com/pankassi/backend/controller/web/WebAuthenticationController.java
+++ backend/src/main/java/com/pankassi/backend/controller/web/WebAuthenticationController.java
@@ -1,4 +1,42 @@
 package com.pankassi.backend.controller.web;
 
+
+import com.pankassi.accesscore.dto.request.LoginRequest;
+import com.pankassi.accesscore.dto.response.AuthenticationResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pankassi.backend.dto.request.RegisterWebRequest;
+import com.pankassi.backend.service.WebAuthenticationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/api/authentication/web")
+@RequiredArgsConstructor
 public class WebAuthenticationController {
+    private final WebAuthenticationService webAuthenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@Valid @RequestBody RegisterWebRequest request) {
+        webAuthenticationService.registerWebUser(request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Inscription ok. Your account is pending of the Administrator validation");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> loginPatient(@Valid @RequestBody LoginRequest request) {
+        AuthenticationResponse response = webAuthenticationService.loginPatient(request);
+        return ResponseEntity.ok(response);
+    }
+
 }
