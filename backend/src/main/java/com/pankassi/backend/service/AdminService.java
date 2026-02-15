c4bad8f4954ad0fce6f4e9aa4dc336e6fdc9ea40
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-15 12:57:57 -0500

    feat(backend): AdminService implement for Admin logic management

--- backend/src/main/java/com/pankassi/backend/service/AdminService.java
+++ backend/src/main/java/com/pankassi/backend/service/AdminService.java
@@ -1,11 +1,18 @@
 package com.pankassi.backend.service;
 
+import com.pankassi.backend.domain.model.Doctor;
+import com.pankassi.backend.domain.model.RequestStatus;
+import com.pankassi.backend.domain.model.Staff;
+import com.pankassi.backend.domain.model.UserProfile;
 import com.pankassi.backend.domain.repository.DoctorRepository;
 import com.pankassi.backend.domain.repository.PatientRepository;
 import com.pankassi.backend.domain.repository.web.StaffRepository;
 import com.pankassi.backend.domain.repository.web.UserProfileRepository;
+import com.pankassi.backend.dto.response.Admin.AdminDashboardResponse;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +22,23 @@ public class AdminService {
     private final StaffRepository staffRepository;
     private  final UserProfileRepository userProfileRepository;
 
-    
+    // ================= ADMIN DASHBOARD =================
+    public AdminDashboardResponse usersStats(){
+        return new AdminDashboardResponse(
+                (int)patientRepository.count(),
+                (int)doctorRepository.count(),
+                (int)staffRepository.count(),
+                (int)userProfileRepository.countByStatus(RequestStatus.PENDING)
+        );
+    }
+
+    /**
+     * User approbation logic
+     */
+
+    // ================= WEB REGISTRATION =================
+
+    // ================= WEB LOGIN =================
+
+    // ================= ADMIN DASHBOARD =================
 }
