5f9dd384c843708f14e4e5868b4b45c2d17ed05b
Rayane BICABA <rayanebicaba.dev@gmail.com>
2026-02-15 12:56:12 -0500

    feat(backend): UserProfileRepository implement for data interaction

--- backend/src/main/java/com/pankassi/backend/domain/repository/web/UserProfileRepository.java
+++ backend/src/main/java/com/pankassi/backend/domain/repository/web/UserProfileRepository.java
@@ -1,7 +1,15 @@
 package com.pankassi.backend.domain.repository.web;
 
+import com.pankassi.backend.domain.model.RequestStatus;
 import com.pankassi.backend.domain.model.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserProfileRepository extends JpaRepository<UserProfile,Long> {
+    //Number of pending request
+    long countByStatus(RequestStatus status);
+
+    //List of pending request
+    List<UserProfile> findAllByStatus(String status);
 }
